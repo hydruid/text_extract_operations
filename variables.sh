a0dc955235db4fb20254640ae754c53f0e45acb4
hydruid <trey.hunsucker@gmail.com>
2024-11-02 16:46:10 -0500

    Sat Nov  2 04:46:10 PM CDT 2024

--- variables.sh
+++ variables.sh
@@ -13,4 +13,19 @@ if [[ "$*" == *"dfl"* ]]; then
 	sed -i '1d' $destinationfile
 fi
 
+#Categories
+##Remove double spaces
+sed -i "s/  //g" $destinationfile
 
+##Bills
+bills=('"FPB CR CARDINTERNETWEB' 'banana')
+fastfoods=('"SONIC DRIVE IN' 'banana')
+if [[ "$*" == *"cat"* ]]; then
+        for bill in "${bills[@]}"; do
+        sed -i "s/$bill/Bill,$bill/g" $destinationfile
+        done
+        for fastfood in "${fastfoods[@]}"; do
+        sed -i "s/$fastfood/FastFood,$fastfood/g" $destinationfile
+        done
+
+fi
