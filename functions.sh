c6a810cba1ac60e3845d297d63f66fbe3b8312bc
hydruid <trey.hunsucker@gmail.com>
2024-11-04 10:19:57 -0600

    Mon Nov  4 10:19:57 AM CST 2024

---
+++ functions.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+#Variables
+source "variables.sh"
+
+#Cleanup and Copy File
+rm -fr $destinationfile
+cp $sourcefile $destinationfile
+
+#Delete Lines that start with Credit
+sed -i '/CREDIT,/d' $destinationfile
+
+#Delete First Line
+delete_first_line () {
+sed -i '1d' $destinationfile
+}
+export -f delete_first_line
+
+#Remove double spaces
+sed -i "s/  //g" $destinationfile
+
+#Remove special Charachters
+sed -i "s/*//g" $destinationfile 
+sed -i "s/'//g" $destinationfile
